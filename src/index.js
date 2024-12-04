f2d0aa280f0581dcc5a250bb05527dab1de6e9e0
EricaAkos <ericadeku4@gmail.com>
2024-12-04 09:27:16 +0000

    added all files

---
+++ src/index.js
@@ -0,0 +1,29 @@
+let name = Erica;
+console.log(Name);
+
+let age = 22;
+console.log(age);
+
+let sum = 10 + 5;
+console.log(sum);
+
+let difference = 20 - 10;
+console.log(difference);
+
+let quotient = 35 / 5;
+console.log(quotient);
+
+let product = 5 * 4;
+console.log(product);
+
+let remainder = 15 % 3;
+console.log(remainder);
+
+let girl = true;
+console.log(girl);
+
+let number =30;
+console.log(typeof number);
+
+let string = Erica;
+console.log(typeof string);
